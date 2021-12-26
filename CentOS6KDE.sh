f71fe805f213b7ac8ae09fc798b6504875606f9e
cdnf <>
2021-12-26 13:31:42 +0800

    update
    clean something is not applicable

--- CentOS6KDE.sh
+++
@@ -1,7 +0,0 @@
-yum groupinstall "X Window System" "KDE Desktop" Desktop -y
-yum install vnc-server -y
-vncpasswd
-vncserver
-chmod +x ~/.vnc/xstartup
-service vncserver restart
-chkconfig vncserver on
--- gitCloneBlog.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/cndf/cndf.github.io.git /tmp/blog
-cd /www/wwwroot/default/
-rm -rf * && mv -f /tmp/blog/.git ./
-git reset --hard
-rm -rf /tmp/blog
